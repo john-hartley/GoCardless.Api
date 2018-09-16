bf5a375e42189411cac5f2cee8d44161721e8c8d
John Hartley <john.hartley4@gmail.com>
2018-09-16 19:18:17 +0100

    Added convenient paging for events.

--- src/GoCardless.Api/Events/GetEventsRequest.cs
+++ src/GoCardless.Api/Events/GetEventsRequest.cs
@@ -3,8 +3,13 @@ using System;
 
 namespace GoCardless.Api.Events
 {
-    public class AllEventsRequest : IPageRequest
+    public class GetEventsRequest : IPageRequest, ICloneable
     {
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
         public string Before { get; set; }
         public string After { get; set; }
         public int? Limit { get; set; }
