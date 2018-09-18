7822d44c7ed9c2b30d462115e949a22e1b509a6c
John Hartley <john.hartley4@gmail.com>
2018-09-18 22:01:21 +0100

    Added XML documentation for the affected dates.

--- src/GoCardless.Api/Payments/CreatePaymentRequest.cs
+++ src/GoCardless.Api/Payments/CreatePaymentRequest.cs
@@ -10,8 +10,18 @@ namespace GoCardless.Api.Payments
         public int Amount { get; set; }
         public int? AppFee { get; set; }
 
+        /// <summary>
+        /// A future date on which the payment should be collected. If not specified, 
+        /// the payment will be collected as soon as possible. This must be on or after 
+        /// the mandate’s next possible charge date, and will be rolled-forwards by 
+        /// GoCardless if it is not a working day.
+        /// <para />
+        /// A calendar date in the ISO-8061 format of yyyy-MM-dd. If a time component is supplied,
+        /// it will be discarded (e.g. 2018-09-18T15:05:06.123Z will become 2018-09-18).
+        /// </summary>
         [JsonConverter(typeof(IsoDateJsonConverter), DateFormat.IsoDateFormat)]
         public DateTime? ChargeDate { get; set; }
+
         public string Currency { get; set; }
         public string Description { get; set; }
 
--- src/GoCardless.Api/Subscriptions/CreateSubscriptionRequest.cs
+++ src/GoCardless.Api/Subscriptions/CreateSubscriptionRequest.cs
@@ -13,6 +13,13 @@ namespace GoCardless.Api.Subscriptions
         public string Currency { get; set; }
         public int? DayOfMonth { get; set; }
 
+        /// <summary>
+        /// Date on or after which no further payments should be created. If this field is blank 
+        /// and count is not specified, the subscription will continue indefinitely.
+        /// <para />
+        /// A calendar date in the ISO-8061 format of yyyy-MM-dd. If a time component is supplied,
+        /// it will be discarded (e.g. 2018-09-18T15:05:06.123Z will become 2018-09-18).
+        /// </summary>
         [Obsolete("Deprecated: This field will be removed in a future API version. Use the Count property to specify a number of payments instead.")]
         [JsonConverter(typeof(IsoDateJsonConverter), DateFormat.IsoDateFormat)]
         public DateTime? EndDate { get; set; }
@@ -28,6 +35,14 @@ namespace GoCardless.Api.Subscriptions
         public string Name { get; set; }
         public string PaymentReference { get; set; }
 
+        /// <summary>
+        /// The date on which the first payment should be charged. Must be within one year of 
+        /// creation and on or after the mandate’s next possible charge date. When blank, this 
+        /// will be set as the mandate’s next possible charge date.
+        /// <para />
+        /// A calendar date in the ISO-8061 format of yyyy-MM-dd. If a time component is supplied,
+        /// it will be discarded (e.g. 2018-09-18T15:05:06.123Z will become 2018-09-18).
+        /// </summary>
         [JsonConverter(typeof(IsoDateJsonConverter), DateFormat.IsoDateFormat)]
         public DateTime? StartDate { get; set; }
     }
