e7189dbf3d207611b89382aab561aa1d20c21cae
John Hartley <john.hartley4@gmail.com>
2020-10-10 18:44:10 +0100

    Added a constructor overload to CustomerBankAccountsClient that accepts an API configuration.

--- src/GoCardless.Api/CustomerBankAccounts/CustomerBankAccountsClient.cs
+++ src/GoCardless.Api/CustomerBankAccounts/CustomerBankAccountsClient.cs
@@ -11,7 +11,17 @@ namespace GoCardless.Api.CustomerBankAccounts
 
         public CustomerBankAccountsClient(IApiClient apiClient)
         {
-            _apiClient = apiClient;
+            _apiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+        }
+
+        public CustomerBankAccountsClient(ApiClientConfiguration apiClientConfiguration)
+        {
+            if (apiClientConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(apiClientConfiguration));
+            }
+
+            _apiClient = new ApiClient(apiClientConfiguration);
         }
 
         public IPagerBuilder<GetCustomerBankAccountsOptions, CustomerBankAccount> BuildPager()
