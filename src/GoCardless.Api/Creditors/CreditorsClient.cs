8d5ce903a71c16e1242c94b0705df9825efe99fe
John Hartley <john.hartley4@gmail.com>
2020-10-10 18:42:38 +0100

    Added a constructor overload to CreditorsClient that accepts an API configuration.

--- src/GoCardless.Api/Creditors/CreditorsClient.cs
+++ src/GoCardless.Api/Creditors/CreditorsClient.cs
@@ -11,7 +11,17 @@ namespace GoCardless.Api.Creditors
 
         public CreditorsClient(IApiClient apiClient)
         {
-            _apiClient = apiClient;
+            _apiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+        }
+
+        public CreditorsClient(ApiClientConfiguration apiClientConfiguration)
+        {
+            if (apiClientConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(apiClientConfiguration));
+            }
+
+            _apiClient = new ApiClient(apiClientConfiguration);
         }
 
         public IPagerBuilder<GetCreditorsOptions, Creditor> BuildPager()
