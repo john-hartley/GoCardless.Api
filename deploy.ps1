5cc0ed4c6d0361452519f0b0c6c16b5afefdddaa
John Hartley <john.hartley4@gmail.com>
2020-10-13 18:04:04 +0100

    Print all environment variables.

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,8 @@
 Write-Host "Pull request: $env:APPVEYOR_PULL_REQUEST_IS_PULL_REQUEST"
 Write-Host "Branch: $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH"
 
+Get-ChildItem Env:*
+
 if ($env:APPVEYOR_PULL_REQUEST_IS_PULL_REQUEST)
 {        
 	Write-Host "Skipping deploy."
