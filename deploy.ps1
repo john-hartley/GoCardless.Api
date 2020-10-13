784c0b1dcdb65ddff9485c2b7933929f85ce2bac
John Hartley <john.hartley4@gmail.com>
2020-10-13 18:01:04 +0100

    Try an actual script.

---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+Write-Host "Pull request: $env:APPVEYOR_PULL_REQUEST_IS_PULL_REQUEST"
+Write-Host "Branch: $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH"
+
+if ($env:APPVEYOR_PULL_REQUEST_IS_PULL_REQUEST)
+{        
+	Write-Host "Skipping deploy."
+	Exit-AppveyorBuild 
+}
