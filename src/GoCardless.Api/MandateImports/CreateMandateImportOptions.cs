efa7cc980cfe1661208ea6e5086dd960abc11bcb
John Hartley <john.hartley4@gmail.com>
2020-10-19 22:31:05 +0100

    Added idempotency key for mandate imports.

--- src/GoCardless.Api/MandateImports/CreateMandateImportOptions.cs
+++ src/GoCardless.Api/MandateImports/CreateMandateImportOptions.cs
@@ -1,7 +1,18 @@
-﻿namespace GoCardlessApi.MandateImports
+﻿using Newtonsoft.Json;
+using System;
+
+namespace GoCardlessApi.MandateImports
 {
     public class CreateMandateImportOptions
     {
+        public CreateMandateImportOptions()
+        {
+            IdempotencyKey = Guid.NewGuid().ToString();
+        }
+
+        [JsonIgnore]
+        public string IdempotencyKey { get; set; }
+
         /// <summary>
         /// See <see cref="Common.Scheme"/> for possible values.
         /// </summary>
